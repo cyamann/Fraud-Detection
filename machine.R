49e84bf5d6a507bd6eb26b8a3b235796e78dd1d8
Ceren Yaman <cerenyaman321123@gmail.com>
2023-12-09 00:15:59 +0300

    faild commit but still

--- machine.R
+++ machine.R
@@ -1,21 +1,51 @@
 library(ranger)
-data <- read.csv("fraud.csv")
+data <- read.csv("C:/project/fraud_subset.csv")
 library(data.table)
 library(caret)
-summary(data)
-names(data)
-#summary of amount
+
+# Assuming "isFraud" is the target variable
+target_variable <- data$isFraud
+
+# Summary of amount
 summary(data$amount)
 sd(data$amount)
 IQR(data$amount)
 var(data$amount)
-#manipulation
+
+# Manipulation
 data$amount <- scale(data$amount)
+
+# Create data2 without the first column and scale amount
+data2 <- data[, -1]
+data2$amount <- scale(data2$amount)
+
 set.seed(12)
-library(caTools)
-data$Class <- as.numeric(data$Class)
-sample_data <- sample.split(data$Class, SplitRatio = 0.80)
-train_data <- subset(data, sample_data=TRUE)
-test_data <- subset(data, sample_data=FALSE)
+install.packages('caTools')
+library('caTools')
+library(reprex)
+
+# Convert "isFraud" to a factor in the original dataset
+data$isFraud <- as.factor(data$isFraud)
+
+# Use the entire data2 dataframe in sample.split
+sample_data <- sample.split(data$isFraud, SplitRatio = 4/5)
+train_data <- subset(data, sample_data == TRUE)
+test_data <- subset(data, sample_data == FALSE)
+dim(train_data)
+dim(test_data)
+#logistik model
+# Assuming your data frame is named "your_data"
+# Replace "your_data" with the actual name of your data frame
+
+# Load necessary library for logistic regression
+library(glmnet)
+data$type <- as.factor(data$type)
+data$nameDest <- as.factor(data$nameDest)
+
+Logistic_Model <- glm(isFraud ~ step + type + amount + nameOrig + oldbalanceOrg + newbalanceOrig +
+                        nameDest + oldbalanceDest + newbalanceDest + isFlaggedFraud,
+                      data = data, family = binomial())
 
 
+
+summary(Logistic_Model)
